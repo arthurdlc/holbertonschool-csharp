72fa965acee4d2b7fd452ed167a52eb63643398c
arthurdlc <delacour.a1109@gmail.com>
2024-12-02 16:38:14 +0100

    frrc

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine($"Percent: {percent:P2}".Replace(" ", ""));
-		Console.WriteLine($"Currency: {currencyy:C2}");
+		Console.WriteLine("Percent: {percent:P2}".Replace(" ", ""));
+		Console.WriteLine("Currency: {currencyy:C2}");
 	}
 }
