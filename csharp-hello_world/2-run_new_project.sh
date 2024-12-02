c0869a3915e9442e7baf0efe938fe5da716aceab
arthurdlc <delacour.a1109@gmail.com>
2024-12-02 15:55:29 +0100

    dff

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 5-print_float
-dotnet build 5-print_float
-dotnet run --project 5-print_float
+dotnet new console -o 6-print_string
+dotnet build 6-print_string
+dotnet run --project 6-print_string
---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        Console.WriteLine($"{str}{str}{str}"); // Affiche la chaîne trois fois
+        Console.WriteLine(str.Substring(0, 9)); // Affiche les 9 premiers caractères
+    }
+}
