a88f3492be8dc61faf314a6ca51aecf1f074f063
arthurdlc <delacour.a1109@gmail.com>
2024-12-02 16:05:51 +0100

    hcyzehs gfvrbvkilqswkd

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 6-print_string
-dotnet build 6-print_string
-dotnet run --project 6-print_string
+dotnet new console -o 7-concat
+dotnet build 7-concat
+dotnet run --project 7-concat
