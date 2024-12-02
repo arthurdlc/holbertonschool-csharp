f80454678097496a35352d24ece5133a31c2fe26
arthurdlc <delacour.a1109@gmail.com>
2024-12-02 13:45:46 +0100

    dcdcd

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 2-new_project
-dotnet build 2-new_project
-dotnet run --project 2-new_project
+dotnet new console -o 3-writeline
+dotnet build 3-writeline
+dotnet run --project 3-writeline
