909fe30c48e578d546b9c1ac7d06842cfe3e610d
Thalie DELACOUR <delacour.a1109@ik.me>
2024-12-02 10:38:41 +0100

    cqfq

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -4,7 +4,8 @@
 if [ -d "1-new_project" ]; then
     echo "The folder '1-new_project' already exists. Please remove or rename it and try again."
     exit 1
+else
+    echo "The folder '1-new_project' does not exist. Creating a new project..."
+    dotnet new console -o 1-new_project
 fi
-
 # Create a new console application project
-dotnet new console -o 1-new_project
