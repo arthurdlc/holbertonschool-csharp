cc08654da6189688d0f27e2d212c3a2cfd73880b
arthurdlc <delacour.a1109@gmail.com>
2024-12-03 13:20:09 +0100

    cddfed

---
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000); // Génère un nombre entre -10000 et 10000
+        int last_digit = number % 10; // Dernier chiffre du nombre
+
+        if (last_digit > 5)
+        {
+            Console.WriteLine($"The last digit of {number} is {last_digit} and is greater than 5");
+        }
+        else if (last_digit == 0)
+        {
+            Console.WriteLine($"The last digit of {number} is {last_digit} and is zero");
+        }
+        else
+        {
+            Console.WriteLine($"The last digit of {number} is {last_digit} and is less than 6 and not 0");
+        }
+    }
+}
---
+++ csharp-ifelse_loops_methods/2-run_new_project.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+dotnet new console -o 1-last_digit
+dotnet build 1-last_digit
+dotnet run --project 1-last_digit
