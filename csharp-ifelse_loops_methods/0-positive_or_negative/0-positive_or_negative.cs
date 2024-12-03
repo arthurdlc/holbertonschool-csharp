ca6aa521b10be6bb1baad02bf9ef6d080166abe8
arthurdlc <delacour.a1109@gmail.com>
2024-12-03 10:18:06 +0100

    sdwd

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -4,8 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ne pas modifier cette ligne
         Random rndm = new Random();
-        int number = rndm.Next(-10, 10);
+        int number = rndm.Next(-10, 10); // Génère un nombre aléatoire entre -10 et 10
+
+        // Vérification et affichage des résultats
         if (number > 0)
         {
             Console.WriteLine("{0} is positive", number);
