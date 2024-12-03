3402387dde2ef308942a89806bbf975f8d294313
arthurdlc <delacour.a1109@gmail.com>
2024-12-03 10:38:00 +0100

    sdwd

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -6,19 +6,17 @@ class Program
     {
         Random rndm = new Random();
         int number = rndm.Next(-10, 10);
-
-        // Déterminer si le nombre est positif, négatif ou zéro
         if (number > 0)
         {
-            Console.WriteLine($"{number} is positive"); // Format attendu
+            Console.WriteLine($"{number} is positive"); 
         }
         else if (number < 0)
         {
-            Console.WriteLine($"{number} is negative"); // Format attendu
+            Console.WriteLine($"{number} is negative"); 
         }
         else
         {
-            Console.WriteLine($"{number} is zero"); // Format attendu
+            Console.WriteLine($"{number} is zero"); 
         }
     }
 }
