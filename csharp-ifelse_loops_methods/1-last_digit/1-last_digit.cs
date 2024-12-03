50f5e50743dcc6c9fd44b51834f62886d1d6baba
arthurdlc <delacour.a1109@gmail.com>
2024-12-03 13:27:04 +0100

    cddfed

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -6,20 +6,22 @@ class Program
     {
         Random rndm = new Random();
         int number = rndm.Next(-10000, 10000);
-        int last_digit = number % 10;
 
-        // Vérification avec if-else
-        if (last_digit > 5)
+        int lastDigit = Math.Abs(number % 10); // On calcule le dernier chiffre absolu
+
+        Console.Write($"The last digit of {number} is {lastDigit} and is "); // Message de base
+
+        if (lastDigit > 5)
         {
-            Console.WriteLine($"The last digit of {number} is {last_digit} and is greater than 5");
+            Console.WriteLine("greater than 5");
         }
-        else if (last_digit == 0)
+        else if (lastDigit == 0)
         {
-            Console.WriteLine($"The last digit of {number} is {last_digit} and is zero");
+            Console.WriteLine("0");
         }
         else
         {
-            Console.WriteLine($"The last digit of {number} is {last_digit} and is less than 6 and not 0");
+            Console.WriteLine("less than 6 and not 0");
         }
     }
 }
