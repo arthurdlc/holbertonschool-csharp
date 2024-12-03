887bc3bee42e916292a6052cd815089c9b4119b1
arthurdlc <delacour.a1109@gmail.com>
2024-12-03 10:35:08 +0100

    sdwd

--- csharp-ifelse_loops_methods/0-positive_or_negative/obj/Debug/net8.0/0-positive_or_negative.AssemblyInfo.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/obj/Debug/net8.0/0-positive_or_negative.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0-positive_or_negative")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386b25dc51149bccf9f07551512e8d47391595f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d61bce8235e3710700cf86e3e058d29b9bbc3ae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("0-positive_or_negative")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0-positive_or_negative")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
