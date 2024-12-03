ec9b393c7fc117b66be97a1aa12451ebb1e1b488
arthurdlc <delacour.a1109@gmail.com>
2024-12-03 13:38:56 +0100

    dcds

--- csharp-ifelse_loops_methods/2-run_new_project.sh
+++ csharp-ifelse_loops_methods/2-run_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 1-last_digit
-dotnet build 1-last_digit
-dotnet run --project 1-last_digit
+dotnet new console -o 2-print_alphabet
+dotnet build 2-print_alphabet
+dotnet run --project 2-print_alphabet
