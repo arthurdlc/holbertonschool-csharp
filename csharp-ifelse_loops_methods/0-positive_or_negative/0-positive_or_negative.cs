9bbe8f5e7f62e5bc76d8a42ea1129a9a188eadee
arthurdlc <delacour.a1109@gmail.com>
2024-12-03 10:08:10 +0100

    sdwd

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+	    switch (number)
+        {
+            case < 0: Console.WriteLin("{0} is negative", number);break;
+            case > 0: Consol.WriteLine("{0} is positive", number);break;
+            default : Console.WriteLine("{0} is zero", number); break;
+        }
+    }
+}
