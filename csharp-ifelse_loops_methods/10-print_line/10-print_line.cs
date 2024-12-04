6fde622caf0bc76f72a4773ffe3405075e512b49
arthurdlc <delacour.a1109@gmail.com>
2024-12-04 14:40:55 +0100

    cdsf

--- csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 
-class Number
+class Line
 {
     public static void PrintLine(int length)
     {
-        if (length < 0)
+        // If length is positive, print underscores
+        if (length > 0)
         {
-            length = 0;
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("_");
+            }
         }
-        for (int i = 0; i < length; i++) // Initialisation, condition, et incrémentation correctes
-        {
-            Console.Write("_");
-        }
-        Console.WriteLine("$"); // Ajoute un symbole `$` à la fin de la ligne
+        // Print a new line at the end
+        Console.WriteLine();
     }
 }
