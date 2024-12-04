dec958add52eec6e04c6921f3b283216a30cd7a1
arthurdlc <delacour.a1109@gmail.com>
2024-12-04 09:39:12 +0100

    cdsf

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -4,14 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 100; i++) // Parcourt les nombres de 0 à 98
+        for (int i = 0; i < 100; i++) // Parcourt les nombres de 0 à 99
         {
-            if (i==99){
-                Console.Write($"{i:D2}");
-            }
-            else{
-                Console.Write($"{i:D2}, ");
-            }
+            if (i == 99)
+                Console.Write($"{i:D2}\n"); // Dernier élément suivi d'une nouvelle ligne
+            else
+                Console.Write($"{i:D2}, "); // Autres éléments suivis de ", "
         }
     }
 }
