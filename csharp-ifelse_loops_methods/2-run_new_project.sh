b5f881079da663131017da00a06716533c2cb70f
arthurdlc <delacour.a1109@gmail.com>
2024-12-04 10:46:42 +0100

    cdsf

--- csharp-ifelse_loops_methods/2-run_new_project.sh
+++ csharp-ifelse_loops_methods/2-run_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 6-print_comb2
-dotnet build 6-print_comb2
-dotnet run --project 6-print_comb2
+dotnet new console -o 7-islower
+dotnet build 7-islower
+dotnet run --project 7-islower
---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        int asciiValue = (int)c; // Conversion du caractère en code ASCII
+        if (asciiValue >= 97 && asciiValue <= 122) // Vérifie si c'est une lettre minuscule
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
