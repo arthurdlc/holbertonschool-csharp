121bcdefcdd5069c35367e5149a6ea2c8df5b0bf
arthurdlc <delacour.a1109@gmail.com>
2024-12-04 13:55:26 +0100

    cdsf

--- csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -8,10 +8,20 @@ class Number
         {
             length = 0;
         }
-        for (int i = 0; i < length; i++)
+        for (int i = 0; i < length; i++) // Initialisation, condition, et incrémentation correctes
         {
             Console.Write("_");
         }
-        Console.WriteLine("$");
+        Console.WriteLine("$"); // Ajoute un symbole `$` à la fin de la ligne
+    }
+}
+
+// Exemple d'utilisation
+class Program
+{
+    static void Main()
+    {
+        Number.PrintLine(5); // Produit "_____$"
+        Number.PrintLine(-3); // Produit "$" (car length est réglé à 0)
     }
 }
