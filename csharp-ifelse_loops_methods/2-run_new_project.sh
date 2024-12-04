433dbaf195bc5ea7d8c9a595b65a9fbce964a7e3
arthurdlc <delacour.a1109@gmail.com>
2024-12-04 13:52:33 +0100

    cdsf

--- csharp-ifelse_loops_methods/2-run_new_project.sh
+++ csharp-ifelse_loops_methods/2-run_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 9-add
-dotnet build 9-add
-dotnet run --project 9-add
+dotnet new console -o 10-print_line
+dotnet build 10-print_line
+dotnet run --project 10-print_line
