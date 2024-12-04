b41a3a2b33d3bb67c24c0e9a66acb3dd3150eaf1
arthurdlc <delacour.a1109@gmail.com>
2024-12-04 10:54:04 +0100

    cdsf

--- csharp-ifelse_loops_methods/2-run_new_project.sh
+++ csharp-ifelse_loops_methods/2-run_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 7-islower
-dotnet build 7-islower
-dotnet run --project 7-islower
+dotnet new console -o 8-print_last_digit
+dotnet build 8-print_last_digit
+dotnet run --project 8-print_last_digit
---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-printlast_digit.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)    {
+        int last_digit = number % 10; // Dernier chiffre (conserve le signe)
+        return last_digit;
+    }
+}
