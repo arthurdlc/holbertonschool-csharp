33bb11074f9af86be6b64aeef2b72ca757a72ce5
arthurdlc <delacour.a1109@gmail.com>
2024-12-04 10:57:24 +0100

    cdsf

--- csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -2,8 +2,10 @@
 
 class Number
 {
-    public static int PrintLastDigit(int number)    {
-        int last_digit = number % 10; // Dernier chiffre (conserve le signe)
-        ConsoleWrite(last_digit);
+    public static int PrintLastDigit(int number)
+    {
+        int last_digit = Math.Abs(number % 10); // Dernier chiffre (valeur absolue pour éviter les signes négatifs)
+        Console.Write(last_digit); // Affiche le dernier chiffre
+        return last_digit; // Retourne le dernier chiffre
     }
 }
