b032deb770eed643170b1791763ba8b834b57c41
arthurdlc <delacour.a1109@gmail.com>
2024-12-07 19:51:26 +0100

    cdsf

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        // Check for invalid size
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        // Create the array
+        int[] newArray = new int[size];
+
+        // Populate and print the array
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+            Console.Write(i);
+            if (i < size - 1)
+                Console.Write(" "); // Add a space between numbers
+        }
+
+        // Print a blank line if the size is 0 or after printing the array
+        Console.WriteLine();
+
+        return newArray;
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/2-run_new_project.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+dotnet new console -o 0-print_array
+dotnet build 0-print_array
+dotnet run --project 0-print_array
