0ef7131f34bc3f1ff19c44debbb7a3fcc64e5831
arthurdlc <delacour.a1109@gmail.com>
2024-12-07 19:59:00 +0100

    cdsf

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        // Check if the index is out of range
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+
+        // Return the element at the given index
+        return array[index];
+    }
+}
--- csharp-arrays_lists_dictionaries/2-run_new_project.sh
+++ csharp-arrays_lists_dictionaries/2-run_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 0-print_array
-dotnet build 0-print_array
-dotnet run --project 0-print_array
+dotnet new console -o 1-element_at
+dotnet build 1-element_at
+dotnet run --project 1-element_at
