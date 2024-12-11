261353c38f16a7dad0db5c7cf37256906cd3568e
arthurdlc <delacour.a1109@gmail.com>
2024-12-11 10:26:04 +0100

    cdsf

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)    
+    {
+        int Nitems = aQueue.Count; // un integer ou on stock le count de la stack
+        Console.WriteLine("Number of items: {0}", Nitems); // on le writreline
+        if (Nitems <= 0)
+            Console.WriteLine("Queue is empty"); // c'est vide on le dit
+        else  
+        {
+            Console.WriteLine("Top item: {0}", aQueue.Peek());//  sinon on print le premier item
+        }
+        aQueue.Enqueue("newItem ");
+        if (!aQueue.Contains(search))
+            Console.WriteLine("Stack contains \"{0}\": False", search);// si il n'y a pas l'element search
+        else
+        {
+            Console.WriteLine("Stack contains \"{0}\": True", search);
+            while (aQueue.Contains(search))
+                aQueue.Dequeue(); //  si il y est on degage tt jusqu'a l'element
+        }
+        aQueue.Push(newItem);
+        return (aQueue); // et on retourne la stack
+    }
+}
