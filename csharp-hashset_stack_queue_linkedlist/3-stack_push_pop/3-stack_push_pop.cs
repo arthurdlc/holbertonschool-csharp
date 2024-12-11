78e59bbc074a3cd8272591f0eb42970b50d6e567
arthurdlc <delacour.a1109@gmail.com>
2024-12-11 10:15:37 +0100

    cdsf

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,62 +1,26 @@
 using System;
 using System.Collections.Generic;
 
-public class MyStack
+class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        // Afficher le nombre d'éléments dans le stack
-        Console.WriteLine($"Number of items: {aStack.Count}");
-
-        // Vérifier si c'est vide
-        if (aStack.Count == 0)
-        {
-            // Si la pile est vide,on envoie ce message 
-            Console.WriteLine("Stack is empty");
-        }
+        int Nitems = aStack.Count; // un integer ou on stock le count de la stack
+        Console.WriteLine("Number of items: {0}", Nitems); // on le writreline
+        if (Nitems <= 0)
+            Console.WriteLine("Stack is empty"); // c'est vide on le dit
+        else  
+            Console.WriteLine("Top item: {0}", aStack.Peek());//  sinon on print le premier item
+        
+        if (!aStack.Contains(search))
+            Console.WriteLine("Stack contains \"{0}\": False", search);// si il n'y a pas l'element search
         else
         {
-            // sinon on affiche le premier element, et on ne le supprime pas 
-            Console.WriteLine($"Top item: {aStack.Peek()}");
+            Console.WriteLine("Stack contains \"{0}\": True", search);
+            while (aStack.Contains(search))
+                aStack.Pop(); //  si il y est on degage tt jusqu'a l'element
         }
-
-        // Vérifier si la pile contient l'élément "search" et si il y est on met pque on l'a trouvé et tt
-        bool containsSearch = aStack.Contains(search);
-        Console.WriteLine($"Stack contains \"{search}\": {containsSearch}");
-
-        // ducp psi il est on strike tt 
-        if (containsSearch)
-        {
-            // Créer une pile temporaire pour stocker les éléments retirés
-            Stack<string> tempStack = new Stack<string>();
-
-            // Retirer les éléments de "aStack" jusqu'à trouver "search"
-            while (aStack.Count > 0)
-            {
-                // Utiliser .Pop() pour retirer le sommet de la pile
-                string item = aStack.Pop();
-
-                // Si l'élément actuel est "search", on arrête
-                if (item == search)
-                {
-                    break;
-                }
-
-                // Ajouter les éléments retirés à la pile temporaire
-                tempStack.Push(item);
-            }
-
-            // Remettre les éléments de la pile temporaire dans "aStack" si nécessaire
-            while (tempStack.Count > 0)
-            {
-                aStack.Push(tempStack.Pop());
-            }
-        }
-
-        // Ajouter le nouvel élément à la pile
         aStack.Push(newItem);
-
-        // Retourner la pile modifiée
-        return aStack;
+        return (aStack); // et on retourne la stack
     }
 }
