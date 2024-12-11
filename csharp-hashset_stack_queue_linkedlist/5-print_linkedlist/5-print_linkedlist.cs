075624878247d3e552f1fc1ec694c1ede5def90d
arthurdlc <delacour.a1109@gmail.com>
2024-12-11 10:39:31 +0100

    cdsf

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> linkedList = new LinkedList<int>();
+        if(size <  0)
+            return linkedList; // on renvoie une liste vide
+
+        for (int i = 0; i < size; i++)
+        {
+            linkedList.AddLast(i);
+            Console.WWriteLine(;);
+        }
+
+        return linkedList;
+    }
+}
