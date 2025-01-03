6a0d42dea6edc3516e693611178741d6522b9e8d
arthurdlc <delacour.a1109@gmail.com>
2025-01-03 10:38:05 +0100

    dcdcd

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+        foreach (int item in myLList)
+        {
+            if (item == value)
+            {
+                return index;
+            }
+            index++;
+        }
+        return -1; // Return -1 if the value is not found
+    }
+}
