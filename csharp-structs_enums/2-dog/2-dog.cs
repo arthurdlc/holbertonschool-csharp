bd489d7099356b5e640a231552a63814826b30d3
arthurdlc <delacour.a1109@gmail.com>
2025-01-03 16:28:34 +0100

    demo

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,25 @@
+using System;
+
+// Define the Rating enum
+enum Rating
+{
+    Good,       // 0
+    Great,      // 1
+    Excellent   // 2
+}
+
+// Define the Dog struct
+struct Dog
+{
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+    public string name;   // nom
+    public float age;     // age
+    public string owner;  // owner
+    public Rating rating; // Dog's rating
+}
