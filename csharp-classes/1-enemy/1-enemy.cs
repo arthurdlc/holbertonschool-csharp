d2f639dc239e9cd5aae88a86cba565ceb693d839
arthurdlc <delacour.a1109@gmail.com>
2025-01-06 10:53:13 +0100

    dede

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -2,13 +2,17 @@ using System;
 
 namespace Enemies
 {
-    
     /// <summary>
     /// Blueprint for zombie.
     /// </summary>
     public class Zombie
     {
+        // Public health field for the zombie
         public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the Zombie class with health set to 0.
+        /// </summary>
         public Zombie()
         {
             health = 0;
