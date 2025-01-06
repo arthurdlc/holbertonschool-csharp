b2cc27e3dbb54e7b581beb912f0021d536cbad06
arthurdlc <delacour.a1109@gmail.com>
2025-01-06 10:44:07 +0100

    dede

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Enemies
 {
-    /// <Summary>Représents a blueprint for zombie</Summary>
+    /// <Summary>Représents a blueprint for zombie</Summary>    
     public class Zombie
     {
     }
