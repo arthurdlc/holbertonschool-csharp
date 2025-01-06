12f8eb3c06996367b2a188d7af97f5c334ec41f0
arthurdlc <delacour.a1109@gmail.com>
2025-01-06 10:51:56 +0100

    dede

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Enemies
+{
+    
+    /// <summary>
+    /// Blueprint for zombie.
+    /// </summary>
+    public class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
