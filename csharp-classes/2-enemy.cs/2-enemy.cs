263fd21c45b12386b5ff11b2ba62087896ecdc89
arthurdlc <delacour.a1109@gmail.com>
2025-01-07 14:16:01 +0100

    demo11

---
+++ csharp-classes/2-enemy.cs/2-enemy.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Blueprint for zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Health of the zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the Zombie class with health set to 0.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Zombie class with a specified health value.
+        /// </summary>
+        /// <param name="value">The initial health value for the zombie. Must be >= 0.</param>
+        /// <exception cref="ArgumentException">Thrown when value is less than 0.</exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+    }
+}
