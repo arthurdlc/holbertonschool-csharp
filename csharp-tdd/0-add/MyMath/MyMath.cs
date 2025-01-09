183eee7a627ee280c67f9da2060365aa2eed86cb
arthurdlc <delacour.a1109@gmail.com>
2025-01-09 15:55:32 +0100

    dcd

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,7 +1,13 @@
 ﻿namespace MyMath
 {
+    /// <summary>
+    /// Class for operations
+    /// </summary>
     public class Operations
     {
+        /// <summary>
+        /// Adds two integers
+        /// </summary>
         public static int Add(int a, int b)
         {
             return a + b;
