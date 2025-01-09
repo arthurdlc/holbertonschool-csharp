d7b06471ad10d9468216d3e37809d93d1594ad4b
arthurdlc <delacour.a1109@gmail.com>
2025-01-09 15:32:27 +0100

    cdcd

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,69 +1,10 @@
-﻿using NUnit.Framework;
-
-namespace MyMath.Tests
+﻿namespace MyMath
 {
-    /// <summary>Class for testing Operations.Add function.</summary>
-    [TestFixture]
-    public class AddTests
+    public class Operations
     {
-        /// <summary>Test when a and b are positives.</summary>
-        [Test]
-        public void TestAetBPositifs()
+        public static int Add(int a, int b)
         {
-            // Arrange
-            int a = 10;
-            int b = 20;
-
-            // Act
-            int output = Operations.Add(a, b);
-
-            // Assert
-            Assert.AreEqual(30, output);
-        }
-
-        /// <summary>Test when a is positive and b is negative.</summary>
-        [Test]
-        public void TestAPositifetBNegatif()
-        {
-            // Arrange
-            int a = 10;
-            int b = -20;
-
-            // Act
-            int output = Operations.Add(a, b);
-
-            // Assert
-            Assert.AreEqual(-10, output);
-        }
-
-        /// <summary>Test when a and b are negatives.</summary>
-        [Test]
-        public void TestAetBNegatifs()
-        {
-            // Arrange
-            int a = -10;
-            int b = -20;
-
-            // Act
-            int output = Operations.Add(a, b);
-
-            // Assert
-            Assert.AreEqual(-30, output);
-        }
-
-        /// <summary>Test when a is equal to zero and b is non equal to zero.</summary>
-        [Test]
-        public void TestANulletBNonNull()
-        {
-            // Arrange
-            int a = 0;
-            int b = 20;
-
-            // Act
-            int output = Operations.Add(a, b);
-
-            // Assert
-            Assert.AreEqual(20, output);
+            return a + b;
         }
     }
 }
