fe688edaf4a5864023ec2bb76e1d86c588b86eee
arthurdlc <delacour.a1109@gmail.com>
2025-01-09 14:59:32 +0100

    dede

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -6,31 +6,20 @@ namespace MyMath.Tests
     [TestFixture]
     public class Tests
     {
-        
-
+                /// <summary>Test when a is equal to zero and b is non equal to zero.</summary>
         [Test]
-        public void Add_TwoPositiveNumbers_ReturnsSum()
+        public void TestANulletBNonNull()
         {
-            Assert.AreEqual(5, Operations.Add(2, 3), "Addition de 2 et 3 devrait donner 5.");
+            // Arrange
+            int a = 0;
+            int b = 20;
+
+            // Act
+            int output = Operations.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(20, output);
         }
 
-        [Test]
-        public void Add_PositiveAndNegativeNumber_ReturnsSum()
-        {
-            Assert.AreEqual(0, Operations.Add(5, -5), "Addition de 5 et -5 devrait donner 0.");
-        }
-
-        [Test]
-        public void Add_TwoNegativeNumbers_ReturnsSum()
-        {
-            Assert.AreEqual(-8, Operations.Add(-3, -5), "Addition de -3 et -5 devrait donner -8.");
-        }
-
-        [Test]
-        public void Add_ZeroAndNumber_ReturnsNumber()
-        {
-            Assert.AreEqual(7, Operations.Add(7, 0), "Addition de 7 et 0 devrait donner 7.");
-            Assert.AreEqual(-4, Operations.Add(0, -4), "Addition de 0 et -4 devrait donner -4.");
-        }
     }
 }
