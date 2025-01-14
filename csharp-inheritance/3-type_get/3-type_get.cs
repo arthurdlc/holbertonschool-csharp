e709d346a5a5c82f7f8ee2c285e1709ab467564a
arthurdlc <delacour.a1109@gmail.com>
2025-01-14 14:01:30 +0100

    deed

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Reflection;
 
 public class Obj
 {
     public static void Print(object myObj)
     {
-        return Console.WriteLine(myObj.GetType());
+        // Get the type of the object
+        Type type = myObj.GetType();
+
+        // Print the name of the type
+        Console.WriteLine("{0} Properties:", type.Name);
+
+        // Get and print the properties
+        PropertyInfo[] properties = type.GetProperties();
+        foreach (var prop in properties)
+        {
+            Console.WriteLine(prop.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", type.Name);
+
+        // Get and print the methods
+        MethodInfo[] methods = type.GetMethods();
+        foreach (var method in methods)
+        {
+            Console.WriteLine(method.Name);
+        }
     }
 }
