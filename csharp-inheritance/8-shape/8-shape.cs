47f8138a037ffc7f82978bdb486e16551bffb3c8
arthurdlc <delacour.a1109@gmail.com>
2025-01-16 11:12:03 +0100

    ddccdc

--- csharp-inheritance/8-shape/8-shape.cs
+++ csharp-inheritance/8-shape/8-shape.cs
@@ -1,38 +1,78 @@
-using System;
-public class Rectangle : Shape
+
+﻿using System;
+
+/// <summary>
+/// Class for shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Calculate the area of a shape.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Class for rectangle
+/// </summary>
+class Rectangle : Shape
 {
     private int width;
     private int height;
-
     public int Width
     {
-        get { return width; }
+        get => width;
         set
         {
-            if (value < 0)
-                throw new ArgumentException("Width must be greater than or equal to 0.");
+            if (value < 0) throw new ArgumentException("Width must be greater than or equal to 0");
             width = value;
         }
     }
-
     public int Height
     {
-        get { return height; }
+        get => height;
         set
         {
-            if (value < 0)
-                throw new ArgumentException("Height must be greater than or equal to 0.");
+            if (value < 0) throw new ArgumentException("Height must be greater than or equal to 0");
             height = value;
         }
     }
-
+/// <summary>
+/// Calculate the area of a rectangle.
+/// </summary>
+/// <returns></returns>
     public new int Area()
     {
         return width * height;
     }
 
+/// <summary>
+/// Return a string representation of the rectangle.
+/// </summary>
+/// <returns></returns>
     public override string ToString()
     {
-        return $"Rectangle: Width={width}, Height={height}, Area={Area()}";
+        return $"[Rectangle] {width} / {height}";
+    }
+}
+
+class Square : Rectangle
+{
+    private int size;
+    public int Size
+    {
+        get => size;
+        set
+        {
+            if (value < 0) throw new ArgumentException("Size must be greater than or equal to 0");
+            size = value;
+            Width = value;
+            Height = value;
+        }
     }
 }
