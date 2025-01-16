d8759b963b0bcf2158d091be6a19879d3740c9fb
arthurdlc <delacour.a1109@gmail.com>
2025-01-16 11:14:42 +0100

    dee

---
+++ csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,88 @@
+
+﻿using System;
+
+/// <summary>
+/// Class for shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Calculate the area of a shape.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Class for rectangle
+/// </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get => width;
+        set
+        {
+            if (value < 0) throw new ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (value < 0) throw new ArgumentException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+/// <summary>
+/// Calculate the area of a rectangle.
+/// </summary>
+/// <returns></returns>
+    public new int Area()
+    {
+        return width * height;
+    }
+
+/// <summary>
+/// Return a string representation of the rectangle.
+/// </summary>
+/// <returns></returns>
+    public override string ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
+    }
+}
+
+class Square : Rectangle
+{
+    private int size;
+    public int Size
+    {
+        get => size;
+        set
+        {
+            if (value < 0) throw new ArgumentException("Size must be greater than or equal to 0");
+            size = value;
+            Width = value;
+            Height = value;
+        }
+    }
+
+    public new int Area()
+    {
+        return size * size;
+    }
+
+    public override string ToString()   
+    {
+        return $"[Square] {size} / {size}";
+    }
+}
