1965bb2a682eee94a11485683ec2d11a2d0d553e
arthurdlc <delacour.a1109@gmail.com>
2025-01-16 10:50:43 +0100

    cd

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,18 +1,39 @@
 using System;
 
+/// <summary>
+/// Class Shape/// 
+/// </summary>
 public class Shape
 {
+    /// <summary>
+    /// Area method
+    /// </summary>
+    /// <returns></returns>
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
     }
 }
 
+/// <summary>
+/// Class Rectangle
+/// </summary>
 public class Rectangle : Shape
 {
+    /// <summary>
+    /// Private fields
+    /// </summary>
     private int width;
     private int height;
 
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
     public int Width
     {
         get { return width; }
@@ -24,6 +45,10 @@ public class Rectangle : Shape
         }
     }
 
+    /// <summary>
+    /// Height///
+    /// </summary>
+    /// <value></value>//  
     public int Height
     {
         get { return height; }
