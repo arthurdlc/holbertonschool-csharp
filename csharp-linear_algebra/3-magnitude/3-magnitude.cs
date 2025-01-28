d8162c6cde5436a887be2e909a7da4b14c7f3192
arthurdlc <delacour.a1109@gmail.com>
2025-01-28 11:27:03 +0100

    ddede

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -2,13 +2,22 @@ using System;
 
 class VectorMath
 {
+    /// <summary>
+    /// Calculate the magnitude of a 2d or 3d vector.
+    /// </summary>
+    /// <param name="vector">double[] vector</param>
+    /// <returns>double</returns>
     public static double Magnitude(double[] vector)
     {
-        double sum = 0;
-        for (int i = 0; i < vector.Length; i++)
+        // Calculate the magnitude
+        if (vector == null || vector.Length == 0)
         {
-            sum += Math.Pow(vector[i], 2);
+            double sum = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                sum += Math.Pow(vector[i], 2);
+            }
+            return Math.Sqrt(sum);
         }
-        return Math.Sqrt(sum);
     }
 }
