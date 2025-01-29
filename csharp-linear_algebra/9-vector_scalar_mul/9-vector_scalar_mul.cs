8d0893054130a7336fb324274d317841dc79d7e0
arthurdlc <delacour.a1109@gmail.com>
2025-01-29 14:20:47 +0100

    cdd

--- csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -6,18 +6,20 @@ using System;
 class VectorMath
 {
     /// <summary>
-    /// Add two 2d / 3d vectors
+    /// Multiplies a 2D or 3D vector by a scalar.
     /// </summary>
-    /// <param name="vector1"></param>
-    /// <param name="vector2"></param>
-    /// <returns></returns>
+    /// <param name="vector">The input vector (2D or 3D).</param>
+    /// <param name="scalar">The scalar value.</param>
+    /// <returns>The resulting vector, or null if the input is invalid.</returns>
     public static double[] Multiply(double[] vector, double scalar)
     {
         if (vector.Length < 2 || vector.Length > 3)
-            return -1;
+            return null; // Return null to indicate an invalid input.
+
         double[] result = new double[vector.Length];
         for (int i = 0; i < vector.Length; i++)
-            result[i] = vector[i]* scalar;
+            result[i] = vector[i] * scalar;
+
         return result;
     }
 }
