a2f8b4e76a2d18777fc4d84f42be6953b251ddf0
arthurdlc <delacour.a1109@gmail.com>
2025-01-29 15:24:43 +0100

    dedcd

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,26 @@
+using System;
+
+/// <summary>
+/// class for matrix math
+/// </summary>
+    class MatrixMath
+    {
+        /// <summary>
+        /// add two matrices
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public static double[,] MultiplyScalar(double[,] matrix, double[,] matrix2)
+        {
+            int matrixRowLength = matrix.GetLength(0);
+            int matrixColLength = matrix.GetLength(1);
+            if ((matrixRowLength != 2 && matrixRowLength != 3))
+                return new double[,] { {-1}};
+            
+            double[,] result = new double[matrixRowLength, matrixColLength];
+            for (int i = 0; i < matrixRowLength; i++)
+                for (int y = 0; y < matrixColLength; y++)
+                    result[i, y] = matrix[i, y] *;
+            return result;
+        }
+    }
