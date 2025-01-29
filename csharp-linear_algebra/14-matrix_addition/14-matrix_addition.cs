a086bf439b9461767bb2db1b993952dff2e2f7f2
arthurdlc <delacour.a1109@gmail.com>
2025-01-29 15:13:08 +0100

    cdc

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,41 +1,30 @@
 using System;
 
 /// <summary>
-/// Class for vector math
+/// class for matrix math
 /// </summary>
-public class VectorMath
-{
-    /// <summary>
-    /// Multiplies two matrices together if dimensions are compatible.
-    /// </summary>
-    /// <param name="matrix1">First matrix (m x n)</param>
-    /// <param name="matrix2">Second matrix (n x p)</param>
-    /// <returns>Resulting matrix (m x p) or {{-1}} if dimensions are invalid</returns>
-    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    class MatrixMath
     {
-        int rowsA = matrix1.GetLength(0);
-        int colsA = matrix1.GetLength(1);
-        int rowsB = matrix2.GetLength(0);
-        int colsB = matrix2.GetLength(1);
-
-        // Vérifier si la multiplication est possible
-        if (colsA != rowsB)
-            return new double[,] { { -1 } }; // Indique une erreur de dimension
-
-        double[,] result = new double[rowsA, colsB];
-
-        for (int i = 0; i < rowsA; i++)  // Parcourt les lignes de A
+        /// <summary>
+        /// add two matrices
+        /// </summary>
+        /// <param name="matrix1"></param>
+        /// <param name="matrix2"></param>
+        /// <returns></returns>
+        public static double[,] Add(double[,] matrix1, double[,] matrix2)
         {
-            for (int j = 0; j < colsB; j++)  // Parcourt les colonnes de B
-            {
-                double sum = 0;
-                for (int k = 0; k < colsA; k++)  // Somme des produits élémentaires
-                {
-                    sum += matrix1[i, k] * matrix2[k, j];
-                }
-                result[i, j] = sum;
-            }
+            int matrix1RowLength = matrix1.GetLength(0);
+            int matrix2RowLength = matrix2.GetLength(0);
+            int matrix1ColLength = matrix1.GetLength(1);
+            int matrix2ColLength = matrix2.GetLength(1);
+
+            if (matrix1RowLength != matrix2RowLength  || matrix1ColLength != matrix2ColLength || (matrix1RowLength != 2 && matrix1RowLength != 3))
+                return new double[,] { {-1}};
+            
+            double[,] result = new double[matrix1RowLength, matrix1ColLength];
+            for (int i = 0; i < matrix1RowLength; i++)
+                for (int y = 0; y < matrix1ColLength; y++)
+                    result[i, y] = matrix1[i, y] + matrix2[i, y];
+            return result;
         }
-        return result;
     }
-}
