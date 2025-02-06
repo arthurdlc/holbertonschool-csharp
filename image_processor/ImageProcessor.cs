27b928cb8f336e0eeee6820a6bfa899a9159e0d9
arthurdlc <delacour.a1109@gmail.com>
2025-02-06 22:38:21 +0100

    cd

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -2,81 +2,92 @@ using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Threading.Tasks;
-using System.Threading;
-using System.Diagnostics;
-using System.Collections.Generic;
 
 class ImageProcessor
 {
+    /// <summary>
+    /// Inverts a list of image(s).
+    /// </summary>
+    /// <param name="filenames">A list of images to invert.</param>
     public static void Inverse(string[] filenames)
     {
-        AsyncFunction(filenames);
-    }
-    private static async Task AsyncFunction(string[] filenames)
-    {
-        await Task.WhenAll(Array.ConvertAll(filenames, async file => await ProcessImageThread(file)));
-    }
-    private static async Task ProcessImageThread(string file_name)
-    {
-        Bitmap bitmap = new Bitmap(file_name);
-
-        BitmapData lockedimage = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
-        int img_size = lockedimage.Stride * lockedimage.Height;
-        byte[] image_copy = new byte[img_size];
-
-        System.Runtime.InteropServices.Marshal.Copy(lockedimage.Scan0, image_copy, 0, img_size);
-
-        for (int i = 0; i < img_size; i++)
-            image_copy[i] = (byte)(255 - image_copy[i]);
-
-        System.Runtime.InteropServices.Marshal.Copy(image_copy, 0, lockedimage.Scan0, img_size);
-        bitmap.UnlockBits(lockedimage);
-
-        string[] slip = file_name.Split(new char[] { '/', '.' });
-        bitmap.Save(slip[slip.Length - 2] + "_inverse." + slip[slip.Length - 1]);
-
-    }
-    public static void Grayscale(string[] filenames)
-    {
-        foreach (var file_name in filenames)
+        foreach (string imagePath in filenames)
         {
-            Bitmap bitmap = new Bitmap(file_name);
-
-            for (int y = 0; y < bitmap.Height; y++)
+            using (Bitmap image = new Bitmap(imagePath))
             {
-                for (int x = 0; x < bitmap.Width; x++)
-                {
-                    Color pixelColor = bitmap.GetPixel(x, y);
-                    int grey = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
-                    bitmap.SetPixel(x, y, Color.FromArgb(grey, grey, grey));
-                }
-            }
+                int width = image.Width;
+                int height = image.Height;
 
-            string[] slip = file_name.Split(new char[] { '/', '.' });
-            bitmap.Save(slip[slip.Length - 2] + "_grayscale." + slip[slip.Length - 1]);
+                BitmapData bmpData = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+                int stride = bmpData.Stride;
+                byte[] pixelBuffer = new byte[stride * height];
+
+                System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+                for (int i = 0; i < pixelBuffer.Length / 4; i++)
+                {
+                    int x = i * 4;
+                    pixelBuffer[x] ^= 0xFF;
+                    pixelBuffer[x + 1] ^= 0xFF;
+                    pixelBuffer[x + 2] ^= 0xFF;
+                }
+
+                System.Runtime.InteropServices.Marshal.Copy(pixelBuffer, 0, bmpData.Scan0, pixelBuffer.Length);
+
+                image.UnlockBits(bmpData);
+
+                string[] nameSplit = imagePath.Split(new char[] { '/', '.' });
+                string newFilename = $"{nameSplit[nameSplit.Length - 2]}_inverse.{nameSplit[nameSplit.Length - 1]}";
+
+                image.Save(newFilename);
+            }
         }
     }
-    public static void BlackWhite(string[] filenames, double threshold)
+
+    /// <summary>
+    /// Converts a list of image(s) to grayscale.
+    /// </summary>
+    /// <param name="filenames">A list of images to convert to grayscale.</param>
+    public static void Grayscale(string[] filenames)
     {
-        Parallel.ForEach(filenames, file_name =>
+        foreach (string imagePath in filenames)
         {
-            Bitmap bitmap = new Bitmap(file_name);
-
-            for (int y = 0; y < bitmap.Height; y++)
+            using (Bitmap image = new Bitmap(imagePath))
             {
-                for (int x = 0; x < bitmap.Width; x++)
-                {
-                    Color pixelColor = bitmap.GetPixel(x, y);
-                    double grey = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
-                    if (grey > threshold)
-                        bitmap.SetPixel(x, y, Color.FromArgb(255, 255, 255));
-                    else
-                        bitmap.SetPixel(x, y, Color.FromArgb(0, 0, 0));
-                }
-            }
+                int width = image.Width;
+                int height = image.Height;
 
-            string[] slip = file_name.Split(new char[] { '/', '.' });
-            bitmap.Save(slip[slip.Length - 2] + "_bw." + slip[slip.Length - 1]);
-        });
+                BitmapData bmpData = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+                int stride = bmpData.Stride;
+                byte[] pixelBuffer = new byte[stride * height];
+
+                System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+                for (int i = 0; i < pixelBuffer.Length / 4; i++)
+                {
+                    int x = i * 4;
+                    byte b = pixelBuffer[x];
+                    byte g = pixelBuffer[x + 1];
+                    byte r = pixelBuffer[x + 2];
+
+                    byte gray = (byte)(0.3 * r + 0.59 * g + 0.11 * b);
+
+                    pixelBuffer[x] = gray;
+                    pixelBuffer[x + 1] = gray;
+                    pixelBuffer[x + 2] = gray;
+                }
+
+                System.Runtime.InteropServices.Marshal.Copy(pixelBuffer, 0, bmpData.Scan0, pixelBuffer.Length);
+
+                image.UnlockBits(bmpData);
+
+                string[] nameSplit = imagePath.Split(new char[] { '/', '.' });
+                string newFilename = $"{nameSplit[nameSplit.Length - 2]}_grayscale.{nameSplit[nameSplit.Length - 1]}";
+
+                image.Save(newFilename);
+            }
+        }
     }
 }
