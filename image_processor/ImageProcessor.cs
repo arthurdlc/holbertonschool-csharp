c5c9f159c5eeb82bb13c521cf0540aa1f2c23a7a
arthurdlc <delacour.a1109@gmail.com>
2025-02-07 16:31:42 +0100

    ded

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -2,76 +2,92 @@ using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Threading.Tasks;
-using System.IO;
-using System.Runtime.InteropServices;
 
 class ImageProcessor
 {
     /// <summary>
-    /// Inverts a list of images in parallel.
+    /// Inverts a list of image(s).
     /// </summary>
     /// <param name="filenames">A list of images to invert.</param>
     public static void Inverse(string[] filenames)
     {
-        Parallel.ForEach(filenames, imagePath =>
+        foreach (string imagePath in filenames)
         {
-            try
+            using (Bitmap image = new Bitmap(imagePath))
             {
-                using (Bitmap image = new Bitmap(imagePath))
+                int width = image.Width;
+                int height = image.Height;
+
+                BitmapData bmpData = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+                int stride = bmpData.Stride;
+                byte[] pixelBuffer = new byte[stride * height];
+
+                System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+                for (int i = 0; i < pixelBuffer.Length / 4; i++)
                 {
-                    int width = image.Width;
-                    int height = image.Height;
-
-                    BitmapData bmpData = image.LockBits(
-                        new Rectangle(0, 0, width, height),
-                        ImageLockMode.ReadWrite,
-                        PixelFormat.Format32bppArgb);
-
-                    int stride = bmpData.Stride;
-                    int byteCount = stride * height;
-                    byte[] pixelBuffer = new byte[byteCount];
-
-                    Marshal.Copy(bmpData.Scan0, pixelBuffer, 0, byteCount);
-
-                    // Inverser uniquement les canaux R, G, B (ne pas toucher à Alpha)
-                    for (int i = 0; i < byteCount; i += 4)
-                    {
-                        pixelBuffer[i] = (byte)(255 - pixelBuffer[i]);       // B
-                        pixelBuffer[i + 1] = (byte)(255 - pixelBuffer[i + 1]); // G
-                        pixelBuffer[i + 2] = (byte)(255 - pixelBuffer[i + 2]); // R
-                        // pixelBuffer[i + 3] => Alpha (on ne le change pas)
-                    }
-
-                    Marshal.Copy(pixelBuffer, 0, bmpData.Scan0, byteCount);
-                    image.UnlockBits(bmpData);
-
-                    string newFilename = Path.Combine(Directory.GetCurrentDirectory(), 
-                        $"{Path.GetFileNameWithoutExtension(imagePath)}_inverse{Path.GetExtension(imagePath)}");
-
-                    // Assurer que le format de sortie est le même que l'original
-                    image.Save(newFilename, GetImageFormat(imagePath));
+                    int x = i * 4;
+                    pixelBuffer[x] ^= 0xFF;
+                    pixelBuffer[x + 1] ^= 0xFF;
+                    pixelBuffer[x + 2] ^= 0xFF;
                 }
+
+                System.Runtime.InteropServices.Marshal.Copy(pixelBuffer, 0, bmpData.Scan0, pixelBuffer.Length);
+
+                image.UnlockBits(bmpData);
+
+                string[] nameSplit = imagePath.Split(new char[] { '/', '.' });
+                string newFilename = $"{nameSplit[nameSplit.Length - 2]}_inverse.{nameSplit[nameSplit.Length - 1]}";
+
+                image.Save(newFilename);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Erreur lors du traitement de {imagePath}: {ex.Message}");
-            }
-        });
+        }
     }
 
     /// <summary>
-    /// Returns the correct ImageFormat based on the file extension.
+    /// Converts a list of image(s) to grayscale.
     /// </summary>
-    private static ImageFormat GetImageFormat(string imagePath)
+    /// <param name="filenames">A list of images to convert to grayscale.</param>
+    public static void Grayscale(string[] filenames)
     {
-        string extension = Path.GetExtension(imagePath).ToLower();
-        return extension switch
+        foreach (string imagePath in filenames)
         {
-            ".png" => ImageFormat.Png,
-            ".jpg" => ImageFormat.Bmp,
-            ".gif" => ImageFormat.Gif,
-            ".tiff" => ImageFormat.Tiff,
-            _ => ImageFormat.Jpeg
-        };
+            using (Bitmap image = new Bitmap(imagePath))
+            {
+                int width = image.Width;
+                int height = image.Height;
+
+                BitmapData bmpData = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+                int stride = bmpData.Stride;
+                byte[] pixelBuffer = new byte[stride * height];
+
+                System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+                for (int i = 0; i < pixelBuffer.Length / 4; i++)
+                {
+                    int x = i * 4;
+                    byte b = pixelBuffer[x];
+                    byte g = pixelBuffer[x + 1];
+                    byte r = pixelBuffer[x + 2];
+
+                    byte gray = (byte)(0.3 * r + 0.59 * g + 0.11 * b);
+
+                    pixelBuffer[x] = gray;
+                    pixelBuffer[x + 1] = gray;
+                    pixelBuffer[x + 2] = gray;
+                }
+
+                System.Runtime.InteropServices.Marshal.Copy(pixelBuffer, 0, bmpData.Scan0, pixelBuffer.Length);
+
+                image.UnlockBits(bmpData);
+
+                string[] nameSplit = imagePath.Split(new char[] { '/', '.' });
+                string newFilename = $"{nameSplit[nameSplit.Length - 2]}_grayscale.{nameSplit[nameSplit.Length - 1]}";
+
+                image.Save(newFilename);
+            }
+        }
     }
 }
