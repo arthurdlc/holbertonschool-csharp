09ed441bea0cc7e59cf48afc7b62bea06799e2e2
arthurdlc <delacour.a1109@gmail.com>
2025-03-24 15:38:12 +0100

    cdc

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,6 +1,31 @@
-﻿abstract class Base(){
-    public string name;
-    public override string ToString(){
-        return  $"{Name} is a {this.GetType().Name}";
+﻿using System;
+
+public abstract class Base
+{
+    // Public property name of type string
+    public string Name { get; set; }
+
+    // Override the ToString() method to return "<name> is a <type>"
+    public override string ToString()
+    {
+        return $"{Name} is a {this.GetType().Name}";
+    }
+}
+
+class SoftwareEngineer : Base
+{
+    // The SoftwareEngineer class does not need any additional implementation for now
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SoftwareEngineer engineer = new SoftwareEngineer();
+
+        engineer.Name = "Betty";  // Setting the name property
+
+        // Output the result of the overridden ToString() method
+        Console.WriteLine(engineer.ToString());  // Expected Output: "Betty is a SoftwareEngineer"
     }
 }
