981c7e34dcdf2ff226dd36df715718cc47a7cd39
arthurdlc <delacour.a1109@gmail.com>
2025-03-24 16:11:51 +0100

    dec

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -2,6 +2,13 @@
 {
     public string Name { get; set; }
 
+/*************  ✨ Codeium Command ⭐  *************/
+/// <summary>
+/// Returns a string representation of the Base instance.
+/// </summary>
+/// <returns>A string in the format: "<name> is a Base".</returns>
+
+/******  6485c18f-fa6e-43c0-b510-5f81c3412e9f  *******/
     public override string ToString()
     {
         // Utilisation de typeof (type statique)
