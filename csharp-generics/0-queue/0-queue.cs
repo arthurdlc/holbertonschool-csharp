7a8feda9c32b1991371dabe0fcf07ae5c01022e1
arthurdlc <delacour.a1109@gmail.com>
2025-03-24 13:56:40 +0100

    de

---
+++ csharp-generics/0-queue/0-queue.cs
@@ -0,0 +1,10 @@
+﻿using system;
+
+public class Queue<T>
+{
+    public void CheckType(T item)
+    {
+        Console.WriteLine("{0} is a {1}", item, typeof(T).Name);
+        //  dans ce cod eon a creer une classe generique qui n'a pas de type
+    }
+}
