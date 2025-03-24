c600226493257f1e03ed4886c9ab442858a8efe6
arthurdlc <delacour.a1109@gmail.com>
2025-03-24 16:20:48 +0100

    dev

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,17 +1,11 @@
-﻿﻿public abstract class Base
+﻿﻿using System;
+
+public abstract class Base
 {
-    public string Name { get; set; }
+    public string name { get; set; }
 
-/*************  ✨ Codeium Command ⭐  *************/
-/// <summary>
-/// Returns a string representation of the Base instance.
-/// </summary>
-/// <returns>A string in the format: "<name> is a Base".</returns>
-
-/******  6485c18f-fa6e-43c0-b510-5f81c3412e9f  *******/
     public override string ToString()
     {
-        // Utilisation de typeof (type statique)
-        return $"{Name} is a {typeof(Base).Name}";  // Renverra toujours "Base"
+        return $"{name} is a {GetType().Name}";
     }
 }
