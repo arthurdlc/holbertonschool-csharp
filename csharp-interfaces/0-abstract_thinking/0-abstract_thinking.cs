ecd687991c60b70a1ee32677b498ad273b6e03d0
arthurdlc <delacour.a1109@gmail.com>
2025-03-24 15:48:17 +0100

    dd

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -3,7 +3,7 @@
 public abstract class Base
 {
     // Public property name of type string
-    public string Name { get; set; }
+    public string Name;
 
     // Override the ToString() method to return "<name> is a <type>"
     public override string ToString()
