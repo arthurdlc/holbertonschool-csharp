2a77da174e4ce5b6b629872a70a18879fe521522
arthurdlc <delacour.a1109@gmail.com>
2025-03-25 10:34:40 +0100

    en sah pitié

--- csharp-generics/1-enqueue/queue.cs
+++ csharp-generics/1-enqueue/queue.cs
@@ -1,9 +1,19 @@
 ﻿﻿using System;
-
+/// <summary>
+/// class for queue
+/// </summary>
 public class Queue<T>
 {
+    /// <summary>
+    /// shear a 2d matrix given a direction and a factor
+    /// </summary>
+    /// <param name="head"></param>
+    /// <param name="tail"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
     public Node<T> head = null;
     public Node<T> tail = null;
+
     private int count = 0;
 
     public Type CheckType()
@@ -11,35 +21,29 @@ public class Queue<T>
         return typeof(T);
     }
 
-    public void Enqueue(T value)
-    {
+    public void Enqueue(T value){
         Node<T> node = new Node<T>(value);
-        if (head == null) // Si la queue est vide
-        {
+        if (head == null){
             head = node;
             tail = node;
         }
-        else
-        {
+        else{
             tail.next = node;
             tail = node;
         }
         count++; // Incrémentation du compteur
     }
 
-    public int Count()
-    {
+    public int Count(){
         return count;
     }
 }
 
-public class Node<T>
-{
+public class Node<T>{
     public T value;  // Ne pas initialiser à null pour compatibilité avec les types valeur
     public Node<T> next = null;
 
-    public Node(T value)
-    {
+    public Node(T value){
         this.value = value;
         this.next = null;
     }
