784ae118674afc685fa391638c4b82580755f26c
arthurdlc <delacour.a1109@gmail.com>
2025-03-25 13:10:47 +0100

    ddcd

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,29 +1,37 @@
 ﻿using System;
+
 public class Player
 {
-    /// <summary>
-    /// propriete du joueur
-    /// name: le nom du joueur
-    /// hp: les hp actuel du joueur
-    /// hpMax: les hp max du joueur
-    /// </summary>
     private string name; 
     private float maxHp;
     private float hp;
 
-    // fonction de constructor, on attribut les proprietes du joueur
-    public void Player(string name, float maxHp) {
+    // Constructeur par défaut
+    public Player() 
+    {
         this.name = "Player";
         this.maxHp = 100f;
-        if (maxHp <= 0f) {
+        this.hp = this.maxHp;
+    }
+
+    // Constructeur avec paramètres
+    public Player(string name, float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp > 0f)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
             this.maxHp = 100f;
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default."); 
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
         this.hp = this.maxHp;
     }
 
-    // cette fonction renvoie un string qui dis ne nom du joueur, et sa vie actuelle par rapport a ses hp max possible
-    public void PrintHealth(string name, float hp, float maxHp) {
+    public void PrintHealth()
+    {
         Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
     }
 }
